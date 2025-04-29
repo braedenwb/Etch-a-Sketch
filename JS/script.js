fcdbbc6f60fd02b9183ba749484e1bc0afc7ba7d
braedenwb <braedenwboyce@gmail.com>
2025-04-29 06:12:43 -0500

    Made the gridsize slider have functionality and the indicator to display the current grid size

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,38 @@ const gridContainer = document.querySelector(".grid-container");
 const gridSlider = document.querySelector(".settings-slider");
 const gridSliderIndicator = document.querySelector(".settings-slider-indicator");
 
+let gridSize = 256;
+
+function getGridSlider()
+{
+    gridSlider.addEventListener("input", () => {
+        if(parseInt(gridSlider.value) === 1)
+        {
+            removeGrid();
+            gridSize = 64;
+            gridSliderIndicator.textContent = "8x8";
+        }
+        else if (parseInt(gridSlider.value) === 2)
+        {
+            removeGrid();
+            gridSize = 144;
+            gridSliderIndicator.textContent = "12x12";
+        }
+        else if (parseInt(gridSlider.value) === 3)
+        {
+            removeGrid();
+            gridSize = 256;
+            gridSliderIndicator.textContent = "16x16";
+        }
+        else
+        {
+            removeGrid();
+            gridSize = 1024;
+            gridSliderIndicator.textContent = "32x32";
+        }
+    })
+}
+
 function createGrid(gridSize)
 {
     for(let i = 0; i < gridSize; i++)
@@ -29,10 +61,21 @@ function createGrid(gridSize)
     }
 }
 
+function removeGrid()
+{
+
+}
+
 function main()
 {
     // 8x8 12x12 16x16 32x32
-    createGrid(144);
+    if(parseInt(gridSlider.value) === 3)
+    {
+        gridSliderIndicator.textContent = "16x16";
+    }
+
+    createGrid(gridSize);
+    getGridSlider();
 }
 
 main();
